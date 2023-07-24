a8fe7ccfe58019848c53fd5622cfe838156ae4c1
yaacinediop <yacinediopkebe@gmail.com>
2023-07-24 13:20:46 +0000

    nike

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Clips from './utils/Clips';
-import SocialLink from './utils/sociallink';
+import SocialLink from './utils/SocialLink';
 const Hero = ({ heroapi: { title, subtitle, btntext, img, sociallinks, videos } }) => {
   // console.log(heroapi)
   return (
